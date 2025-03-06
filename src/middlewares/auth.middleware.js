f77227180610b37cc247951bd684430271864d66
Saakshi-Baranwal <saakshibaranwal@kgpian.iitkgp.ac.in>
2025-03-06 21:19:32 +0530

    add refreshAccessToken endpoint

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,29 +1,60 @@
+// import { apiError } from "../utils/apiError.js";
+// import { asyncHandler } from "../utils/asyncHandler.js";
+// import jwt from 'jsonwebtoken'
+// import { User } from "../models/user.model.js";
+
+
+// export const verifyJWT = asyncHandler(async (req, _, next) => {
+//     try {
+//         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+    
+//         if(!token){
+//             throw new apiError(401, "Unauthorised request")
+//         }
+    
+//         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+//         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+    
+//         if(!user) {
+//             throw new apiError(401, "Invalid access token") 
+//         }
+    
+//         req.user = user;
+//         next();
+//     } catch (error) {
+//         throw new apiError(401, "Invalid ok access token")
+//     }
+
+// })
+
 import { apiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken"
 import { User } from "../models/user.model.js";
 
-
-export const verifyJWT = asyncHandler( async (req, _, next) => {
+export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-    
-        if(!token){
-            throw new apiError(401, "Unauthorised request")
+        
+        console.log(token);
+        if (!token) {
+            throw new apiError(401, "Unauthorized request")
         }
     
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
     
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
     
-        if(!user) {
-            throw new apiError(401, "Invalid access token") 
+        if (!user) {
+            
+            throw new apiError(401, "Invalid Access Token")
         }
     
         req.user = user;
-        next();
+        next()
     } catch (error) {
-        throw new apiError(401, "Invalid access token")
+        throw new apiError(401, error?.message || "Invalid access token")
     }
-
+    
 })
